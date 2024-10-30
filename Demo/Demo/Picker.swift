30474fad6a9927727352c898b47802c139287f35
hieutm3 <hieutm3@vnpay.vn>
2024-10-30 10:51:19 +0700

    stepper

--- Demo/Demo/Picker.swift
+++ Demo/Demo/Picker.swift
@@ -8,16 +8,27 @@
 import SwiftUI
 
 struct PickerUI: View {
-    var colors = ["Red", "Green", "Blue", "Tartan"]
-    @State private var selectedColor: Int = 0
-    var body: some View {
-        VStack {
-            Picker("choose", selection: $selectedColor) {
-                ForEach(0..<colors.count, id: \.self) { index in
-                    Text(colors[index])
-                }
-            }
-        }.pickerStyle(SegmentedPickerStyle())
+//    var colors = ["Red", "Green", "Blue", "Tartan"]
+//    @State private var selectedColor: Int = 0
+//    var body: some View {
+//        VStack {
+//            Picker("choose", selection: $selectedColor) {
+//                ForEach(0..<colors.count, id: \.self) { index in
+//                    Text(colors[index])
+//                }
+//            }
+//            
+//        }.pickerStyle(SegmentedPickerStyle())
+//        Text("Choose: \(colors[selectedColor])")
+//    }
+    @State var x = 20
+    var body: some View{
+        // cACSH 1
+//        Stepper("Enter value", value: $x, in: 0...120)
+//        Text("value \(x)")
+        // cACH 2:
+        Stepper("Enter value", value: $x, in: 0...100, step: 5)
+                    Text("Value: \(x)")
     }
 }
 
